1ac3f79a9128f645b39aabc4357091dbb7a46003
king-oyster-pumpkin-soup-seasonings <283332318+king-oyster-pumpkin-soup-seasonings@users.noreply.github.com>
2026-08-03 10:41:32 +0800

    feat(gameplay): setup spawners for weapons and powerups with randomized spawn points

---
+++ Assets/Scripts/WeaponSpawner.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class WeaponSpawner : MonoBehaviour
+{
+    [SerializeField] private float spawnInterval;
+    [SerializeField] private GameObject[] weapons;
+    private float time;
+
+    void Start()
+    {
+        if (spawnInterval == 0) spawnInterval = 1f;
+        time = 0;
+    }
+
+    void Update()
+    {
+        if (GameManager.Instance.isMessageDone == false) return;
+
+        if (time < spawnInterval)
+        {
+            time += Time.deltaTime;
+        }
+        else
+        {
+            transform.position = new Vector2(Random.Range(-9f, 9f), transform.position.y);
+            time = 0;
+            // Instantiate(weapons[0], transform.position, transform.rotation);
+        }
+    }
+}
